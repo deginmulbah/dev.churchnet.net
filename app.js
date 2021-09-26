8b9b078fee8d0a4a127b19b8e3f4d12c84cf54eb
Jenkins D.Mulbah <48864692+deginmulbah@users.noreply.github.com>
2021-09-26 10:08:04 -0700

    modifed env file

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-require('dotenv').config({
-    path: `./env-files/${'development'}.env`,
-});
+require('dotenv').config();
 const path = require('path');
 const helmet = require("helmet"); 
 const express = require('express');
